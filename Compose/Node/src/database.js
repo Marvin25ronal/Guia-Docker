f080749f11338736b71aa5c77ba8367ba73141cd
Marvin <marvin1ronal@gmail.com>
2020-07-07 13:56:18 -0600

    Compose

---
+++ Compose/Node/src/database.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+
+mongoose.connect('mongodb://mongo/prueba',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+    .then(db=>console.log('Db is connected to ',db.connection.host))
+    .catch(err=>console.error(err))
+
