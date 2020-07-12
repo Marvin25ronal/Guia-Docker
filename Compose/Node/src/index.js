97d7af4514a9c6ad444cd18110449c2a36203e95
Marvin <marvin1ronal@gmail.com>
2020-07-12 13:49:33 -0600

    cierre

--- Compose/Node/src/index.js
+++ Compose/Node/src/index.js
@@ -1,9 +1,12 @@
 const express=require('express')
-
+var bodyParser = require('body-parser');
+var cors = require('cors');
 const app=express();
+app.use(cors())
 
 
-
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(require('./routes/index.routes'))
 app.listen(3000)
 
--- Compose/Node/src/routes/index.routes.js
+++ Compose/Node/src/routes/index.routes.js
@@ -1,14 +1,13 @@
 const { Router } = require('express')
 const router = Router()
 const mongoose = require('mongoose')
-
+var cors = require('cors')
 mongoose.connect('mongodb://mongo/prueba', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(db => console.log('Db is connected to ', db.connection.host))
     .catch(err => console.error(err))
-
 
 
 const Schema = mongoose.Schema;
@@ -36,5 +35,27 @@ router.get('/users', (req, res) => {
     });
     // res.send('hola')
 })
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+router.post('/insertar', cors(corsOptions), (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    let json = req.body.name
+    var nuevo = new MyModel({ name: json })
+    nuevo.save((err) => {
+        if (err) {
+            res.json({ message: false })
+        }
+        res.json({ message: true })
+    })
 
+})
 module.exports = router
